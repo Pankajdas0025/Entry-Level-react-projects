1c09597721e5813707ad72fa4c534b1cee0d46a7
PANKAJ KUMAR DAS <pankajkumardas8678@gmail.com>
2026-01-23 17:46:13 +0530

    Add a new Basic React Projects

--- Quiz-app/src/App.jsx
+++ Quiz-app/src/App.jsx
@@ -1,35 +1,34 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {BrowserRouter , Routes, Route, Link} from 'react-router-dom'
+import {Suspense, Lazy} from 'react'
+import Home from './pages/Home'
+import Quiz from './pages/Quiz'
+import LeaderBoard from './pages/LeaderBoard'
+
+const Result = Lazy(() => import('./pages/Result')) //Lazy is not a function error fixed
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <>
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Home/>}/>
+                <Route path='/quiz' element={<Quiz/>}/>
+                <Route path='/result' element={<Suspense fallback={<div>Loading...</div>}> <Result/> </Suspense>}/>
+                <Route path='/leaderboard' element={<LeaderBoard/>}/>
+              </Routes>
+        </BrowserRouter>
+      </>
   )
 }
 
 export default App
+
+
+
+
+
+
+
